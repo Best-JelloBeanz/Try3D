69220f8a50ed411be4b17f0e6fd06de474017618
Best-JelloBeanz <93356778+Best-JelloBeanz@users.noreply.github.com>
2023-04-25 14:47:01 -0500

    Update Launcher.java
    A blank window should theoretically launch now

--- src/main/java/com/jello/test/Launcher.java
+++ src/main/java/com/jello/test/Launcher.java
@@ -1,9 +1,17 @@
 package com.jello.test;
 
+import com.jello.core.WindowManager;
 import org.lwjgl.Version;
 
 public class Launcher {
     public static void main(String[] args){
         System.out.println(Version.getVersion());
+		WindowManager window = new WindowManager("JELLO ENGINE", 1600, 900, false);
+		window.init();
+		
+		while(!window.windowSHouldClose()) {
+			window.update();
+		}
+		window.cleanup();
     }
 }
